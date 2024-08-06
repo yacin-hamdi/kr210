a342e5a50bf7e07dcd27b73be651d5551998c98d
yacin-hamdi <yacin.ha9@gmail.com>
2024-08-06 17:44:37 +0100

    create a launch file that open rviz and robot state publisher

---
+++ build/kr210_bringup/CMakeFiles/kr210_bringup_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kr210_bringup_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/kr210_bringup_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/kr210_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/moose/ros2_ws/src/kr210_bringup
+# Build directory: /home/moose/ros2_ws/build/kr210_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
