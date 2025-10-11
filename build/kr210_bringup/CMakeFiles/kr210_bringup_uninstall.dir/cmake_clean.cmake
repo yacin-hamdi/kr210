63e6d750f527f46ff90ba1ff0956a900459fed4f
yacin <yacin.ha9@gmail.com>
2025-10-11 20:19:43 +0100

    remove unnecessary files

--- build/kr210_bringup/CMakeFiles/kr210_bringup_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/kr210_bringup_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/kr210_bringup_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/kr210_bringup/CMakeFiles/kr210_bringup_uninstall.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for kr210_bringup_uninstall.
