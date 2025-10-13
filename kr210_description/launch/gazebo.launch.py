8bb066cb55b7003efd180b544adc2f417bbe1c3e
yacin <yacin.ha9@gmail.com>
2025-10-13 17:39:07 +0100

    add gazebo launch file

---
+++ kr210_description/launch/gazebo.launch.py
@@ -0,0 +1,71 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable, DeclareLaunchArgument
+from launch.substitutions import PathJoinSubstitution, PythonExpression, LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+from pathlib import Path
+import os
+
+def generate_launch_description():
+
+    world_name_arg = DeclareLaunchArgument(
+        name="world_name",
+        default_value="empty"
+    )
+    world_name = LaunchConfiguration("world_name")
+
+    world_path = PathJoinSubstitution([
+        FindPackageShare("kr210_description"),
+        "worlds", 
+        PythonExpression(["'", world_name, "'", " + '.world'"])
+    ])
+
+    
+    
+
+    resource_path = str(Path(get_package_share_directory("kr210_description")).parent.resolve())
+    resource_path += os.pathsep + os.path.join(get_package_share_directory("kr210_description"), "models")
+    gazebo_resource_path = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH", 
+        value=resource_path
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                FindPackageShare("ros_gz_sim"),
+                "launch",
+                "gz_sim.launch.py"
+            ])
+        ),
+        # launch_arguments=[("gz_args", PythonExpression(["' -v 4 -r ", world_path, "'"]))]
+        launch_arguments=[("gz_args", PythonExpression(["' -v 4 -r ", "empty.sdf", "'"]))]
+    )
+
+    gz_spawn_entity = Node(
+        package="ros_gz_sim", 
+        executable="create", 
+        output="screen", 
+        arguments=["-topic", "robot_description", 
+                    "-name", "kr210",
+                   ]
+    )
+
+    
+
+    gz_ros2_bridge = Node(
+        package="ros_gz_bridge", 
+        executable="parameter_bridge", 
+        arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+        ]
+    )
+    return LaunchDescription([
+        world_name_arg,
+        gazebo_resource_path,
+        gazebo, 
+        gz_spawn_entity, 
+        gz_ros2_bridge
+    ])
